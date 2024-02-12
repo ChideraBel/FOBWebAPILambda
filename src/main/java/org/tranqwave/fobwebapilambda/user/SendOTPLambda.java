0fb98891a0ea2c9a2a56c8e095913fe65b0f7fe0
chi-ops <cbeluonwu@hawk.iit.edu>
2024-02-12 16:53:15 -0600

    Add api operations for OTP verification

---
+++ src/main/java/org/tranqwave/fobwebapilambda/user/SendOTPLambda.java
@@ -0,0 +1,88 @@
+package org.tranqwave.fobwebapilambda.user;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+import dao.OTPCodesDao;
+import dao.UserDao;
+import dao.dbModels.DynamoDBOTPCode;
+import dao.dbModels.DynamoDBUser;
+import lombok.NonNull;
+import model.ResponseMessage;
+import model.SendOTPRequest;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static utils.ConstantUtils.ERROR;
+import static utils.ConstantUtils.SUCCESS;
+import static utils.CredentialsUtils.COMPANY_EMAIL;
+import static utils.CredentialsUtils.getSecret;
+
+public class SendOTPLambda {
+    private final OTPCodesDao otpCodesDao;
+    private final UserDao userDao;
+
+    public SendOTPLambda(OTPCodesDao otpCodesDao, UserDao userDao) {
+        this.otpCodesDao = otpCodesDao;
+        this.userDao = userDao;
+    }
+
+    /*
+    Generates random OTP code and sends the code to the user's email
+     */
+    public ResponseMessage sendOTPCode(SendOTPRequest request, Context context) {
+        Instant dateInstant = new Date().toInstant();
+        String currentDate = DateTimeFormatter.ISO_INSTANT.format(dateInstant);
+
+        final DynamoDBUser user = userDao.getUser(request.getEmail());
+
+        if (user == null) {
+            return new ResponseMessage(ERROR, "Account with email does not exist");
+        }
+
+        final String randomizedCode = String.valueOf(ThreadLocalRandom.current().nextInt(1000, 9999));
+
+        final boolean response = sendCode(request.getEmail(), randomizedCode);
+
+        if (!response) {
+            return new ResponseMessage(ERROR, "Your OTP code could not be sent!");
+        }
+        final DynamoDBOTPCode dynamoDBOTPCode = DynamoDBOTPCode.builder()
+                .user_id(request.getEmail())
+                .otp_code(randomizedCode)
+                .timestamp(currentDate)
+                .build();
+        otpCodesDao.save(dynamoDBOTPCode);
+
+        return new ResponseMessage(SUCCESS, "Your OTP code has been sent!");
+    }
+
+    private boolean sendCode(@NonNull final String userEmail, @NonNull final String code) {
+        Email from = new Email(COMPANY_EMAIL);
+        String subject = "Your FOB Registration OTP code";
+        Email to = new Email(userEmail);
+        Content content = new Content("text/plain", String.format("Please enter your OTP registration code: %s", code));
+        Mail mail = new Mail(from, subject, to, content);
+
+        SendGrid sg = new SendGrid(getSecret("AppBetaSendGridKeys"));
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            sg.api(request);
+        } catch (IOException ex) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/main/java/utils/CredentialsUtils.java
@@ -0,0 +1,33 @@
+package utils;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
+
+public class CredentialsUtils {
+
+    final public static String COMPANY_EMAIL = "tranqwave@gmail.com";
+    public static String getSecret(String secretName) {
+        Region region = Region.of("us-east-2");
+
+        // Create a Secrets Manager client
+        SecretsManagerClient client = SecretsManagerClient.builder()
+                .region(region)
+                .build();
+
+        GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
+                .secretId(secretName)
+                .build();
+
+        GetSecretValueResponse getSecretValueResponse;
+
+        try {
+            getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
+        } catch (Exception e) {
+            throw e;
+        }
+
+        return getSecretValueResponse.secretString();
+    }
+}
